a7bbfcf7507537ef2a12cc945d2b823b210a93f3
ALEX E. WEIDNER <aeweidne@us.ibm.com>
2017-01-04 14:42:31 -0500

    initial commit

---
+++ src/index.js
@@ -0,0 +1,15 @@
+require('dotenv').config();
+
+const Telegraf = require('telegraf');
+ 
+const app = new Telegraf(process.env.TELEGRAM_TOKEN);
+ 
+app.command('start', (ctx) => {
+  console.log('start', ctx.from);
+  ctx.reply('Welcome!');
+});
+ 
+app.hears('hi', (ctx) => ctx.reply('Hey there!'));
+app.on('sticker', (ctx) => ctx.reply('👍'));
+ 
+app.startPolling();
